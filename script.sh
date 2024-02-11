767a591acadfa44fd3b055061c0727d8cd998ece
= <=>
2024-02-11 15:32:23 +0530

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -1,64 +1,41 @@
 #!/bin/bash
 
-###############################
-## AWS config
-###############################
+<< comment 
+This script is going to encrypt the aws log group of AWS CloudWatch
+comment
+
+# Define AWS region
 aws_region="us-east-1"
-cmk_key_arn="arn:aws:kms:us-east-1:871794273757:key/09276167-1b21-4420-8b32-9fe77c58f6bd"
+aws_cmk_key_id="ebe49f2c-d801-4ab5-93e2-853e381d2523"
 
-
-###############################
-## Function to list log Groups
-###############################
-
+# List CloudWatch log groups
 list_log_groups() {
-    echo "The following CloudWatch log groups will be encrypted using CMK :"
+    echo "The following CloudWatch log groups will be encrypted using CMK Keyz:"
     aws logs describe-log-groups --region $aws_region --query 'logGroups[*].logGroupName' --output table
 }
 
-###############################
-## Encrypt CloudWatch log groups
-###############################
-
+# Encrypt CloudWatch log groups
 encrypt_log_groups() {
     echo "Encrypting CloudWatch log groups..."
+    # Loop through each log group and encrypt
     log_groups=$(aws logs describe-log-groups --region $aws_region --query 'logGroups[*].logGroupName' --output text)
     for log_group in $log_groups; do
-        aws logs associate-kms-key --log-group-name $log_group --kms-key-id $cmk_key_arn
+        aws logs put-retention-policy --region $aws_region --log-group-name $log_group --retention-in-days 30 --kms-key-id $aws_cmk_key_id
     done
     echo "Encryption complete."
 }
 
-###############################
-## Error Handling
-###############################
-set -e
-# function to handle error
-handle_error() {
-    echo "Failed to set retention policy for log group: $log_group"
-    exit 1
-}
-# Trap errors and call the custom function
-trap 'handle_error' ERR
-
-
-###############################
-## Main
-###############################
+# Main script
 list_log_groups
-if [ "$1" == "-y" ]; then
+read -p "Do you want to proceed with encryption? (yes/no): " choice
+case "$choice" in 
+  yes|Yes|YES )
     encrypt_log_groups
-else
-    read -p "Do you want to proceed with encryption? (yes/no): " choice
-    case "$choice" in 
-    yes|Yes|YES )
-        encrypt_log_groups
-        ;;
-    no|No|NO )
-        echo "Encryption aborted."
-        ;;
-    * )
-        echo "Invalid input. Encryption aborted."
-        ;;
-    esac
-fi
+    ;;
+  no|No|NO )
+    echo "Encryption aborted."
+    ;;
+  * )
+    echo "Invalid input. Encryption aborted."
+    ;;
+esac
