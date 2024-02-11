ea5ce2f9284be33ce1f880624de92b4c7d67942d
= <=>
2024-02-11 14:26:09 +0530

    script added

---
+++ script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+###############################
+## AWS config
+###############################
+aws_region="us-east-1"
+cmk_key_arn="arn:aws:kms:us-east-1:871794273757:key/fb71a505-f856-411a-9bff-7aba7d8e34bf"
+
+
+###############################
+## Function to list log Groups
+###############################
+
+list_log_groups() {
+    echo "The following CloudWatch log groups will be encrypted using CMK :"
+    aws logs describe-log-groups --region $aws_region --query 'logGroups[*].logGroupName' --output table
+}
+
+###############################
+## Encrypt CloudWatch log groups
+###############################
+
+encrypt_log_groups() {
+    echo "Encrypting CloudWatch log groups..."
+    log_groups=$(aws logs describe-log-groups --region $aws_region --query 'logGroups[*].logGroupName' --output text)
+    for log_group in $log_groups; do
+        aws logs associate-kms-key --log-group-name $log_group --kms-key-id $cmk_key_arn
+    done
+    echo "Encryption complete."
+}
+
+###############################
+## Error Handling
+###############################
+set -e
+# function to handle error
+handle_error() {
+    echo "Failed to set retention policy for log group: $log_group"
+    exit 1
+}
+# Trap errors and call the custom function
+trap 'handle_error' ERR
+
+
+###############################
+## Main
+###############################
+list_log_groups
+if [ "$1" == "-y" ]; then
+    encrypt_log_groups
+else
+    read -p "Do you want to proceed with encryption? (yes/no): " choice
+    case "$choice" in 
+    yes|Yes|YES )
+        encrypt_log_groups
+        ;;
+    no|No|NO )
+        echo "Encryption aborted."
+        ;;
+    * )
+        echo "Invalid input. Encryption aborted."
+        ;;
+    esac
+fi
